d053a78413d538f612dbd99a96fca20f0b0bc1c4
thumbthing <ilscc0902@gmail.com>
2023-03-23 18:11:50 +0900

    중첩 Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
@@ -9,6 +11,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles/:id" element={<Article />} />
     </Routes>
   );
 };
