9252eb3440be3b4f31f2f576a449a5ab2f24878a
thumbthing <ilscc0902@gmail.com>
2023-03-23 18:23:16 +0900

    fix: App.js (Home, About, Profile Component 에 공통된 해더 적용)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import About from "./pages/About";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
@@ -8,9 +9,11 @@ import Profile from "./pages/Profile";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
