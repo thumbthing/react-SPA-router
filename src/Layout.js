9fc33afe2f5150615230848f2abdcf9831f212b0
thumbthing <ilscc0902@gmail.com>
2023-03-23 18:44:59 +0900

    Layout.js (navigate 적용)

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goArticles = () => {
+    navigate('/articles');
+  };
+
   return (
     <div>
-      <header style={{background: 'lightgray', padding: 16, fontsize: 24}}>
-        header
+      <header style={{background: 'lightgrey', padding: 16, fontSize: 24}}>
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
