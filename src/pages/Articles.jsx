6f7efe1936f6757eb701dffc8e92275dd2ff6f4b
thumbthing <ilscc0902@gmail.com>
2023-03-27 12:56:07 +0900

    Articles.jsx 반복되는 코드 리펙토링

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,42 +1,34 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
-  const activeStyle = {
-    color: 'green',
-    fontsize: 21,
-  };
-
   return (
     <>
       <Outlet />
       <ul>
-        <li>
-          <NavLink
-            to="/articles/1"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 1
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-           to="/articles/2"
-           style={({ isActive }) => (isActive ? activeStyle : undefined)}
-           >
-            게시글 2
-           </NavLink>
-        </li>
-        <li>
-          <NavLink
-           to = "/articles/3"
-           style={({ isActive}) => (isActive ? activeStyle : undefined)}
-           >
-            게시글 3
-           </NavLink>
-        </li>
+        <ArticleItem id = {1} />
+        <ArticleItem id = {2} />
+        <ArticleItem id = {3} />
       </ul>
     </>
   );
 };
 
-export default Articles;
+const ArticleItem = ({ id }) => {
+  const acticeStyle = {
+    color: 'green',
+    fontsize: 21,
+  };
+
+  return (
+    <li>
+      <NavLink
+       to = {`/articles/${id}`}
+       style = {({ isActive }) => (isActive ? acticeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
+  )
+}
+
+export default Articles
