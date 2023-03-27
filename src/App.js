00464e97c8bd98fa3564e38c27a323730d1c1113
thumbthing <ilscc0902@gmail.com>
2023-03-27 13:31:24 +0900

    feat: Mypage.jsx, Login.jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import About from "./pages/About";
-import Articles from "./pages/Articles";
-import Article from "./pages/Article";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import About from './pages/About';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} /> 
       </Route>
-      <Route path="/articles" element={<Articles />}>
-        <Route path=":id" element={<Article />} />
+      <Route path='/articles' element={<Articles />}>
+        <Route paht=':id' element={<Article />} />
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 };
